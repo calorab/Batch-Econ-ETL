b4f74b699bafc6445f1edb6d6bc871e8026e7811
Calorab <calebwgammon@gmail.com>
2023-05-11 12:59:44 -0400

    added 60 sec deley if NOTE message is returned from AV - successful

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from requests.exceptions import HTTPError
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
@@ -23,15 +24,27 @@ def main():
 
             response.raise_for_status()
             data = response.json()
+            for i in data:
+                if i == 'Note':
+                    print(f'60 Second delay for {link}...\n')
+                    time.sleep(31)
+                    print('30 seconds left...')
+                    time.sleep(30)
+                    response, data = []
+                    retry = requests.get(url)
         except HTTPError as http_err:
             print(f'An HTTP error occurred on {link}: {http_err}')
         except Exception as err:
             print(f'There was an error with {link}', err)
         finally:
-            print("DONE!\n")
-            print(" ... ...\n")
-            print(data)
+            if data != []:
+                print(data)
+            else:
+                print('This is a retry: \n', retry)
+
+
+            
 
 main()
 
-# tested as of 5/11 at nopon and all works as expected. Although Alpha Vantage limts API calls to 5 per min so need to adjust process for that
+# tested as of 5/11 at noon and all works as expected. Although Alpha Vantage limts API calls to 5 per min so need to adjust process for that
