ea57c80b90f01df1f1ecb6d35d0aedcf37f0b32f
Calorab <calebwgammon@gmail.com>
2023-05-11 11:19:21 -0400

    testing basic print for Link and URL for each API in pool

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ URL_POOL = ('AV_FOREX_URL','AV_OIL_WTI_URL','AV_COMMODITIES_INDEX_URL','AV_GDP_U
 
 
 def main():
-    print("Inside main.py")
+    print("Inside main()")
     
     for link in URL_POOL:
         print("inside for loop")
@@ -17,17 +17,18 @@ def main():
             pass
         try:
             print("Inside Try")
-            url = os.get_env(link)
-            response = requests.get(url)
+            url = os.getenv(link)
+            print(link, url)
+            # response = requests.get(url)
 
-            response.raise_for_status()
+            # response.raise_for_status()
         except HTTPError as http_err:
             print(f'An HTTP error occurred on {link}: {http_err}')
         except Exception as err:
-            print(f'There was an error with {link} /n', err)
+            print(f'There was an error with {link}', err)
         finally:
             print("DONE!")
 
+main()
 
-
-# The above is untested as of 5/5/23
+# The above is tested SUCCESSFULLY as of 5/10/23
