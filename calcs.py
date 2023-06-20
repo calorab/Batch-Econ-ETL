73824f642b1a98757363b96180594526cd9decbb
Calorab <calebwgammon@gmail.com>
2023-06-20 13:53:38 -0400

    refactored sma and ema into separate funcs for collbacks

--- calcs.py
+++ calcs.py
@@ -6,7 +6,41 @@ import sqlite3
 # ALSO MAY NEED TO BE EDITED FOR NON CANDLE FORMATTED TABLES (i.e date/value format) 
 # Although that would only be for Oil and Commodities Indices which I won't be charting in version 1
 
-def build_calcs_table(table):
+def build_sma_crossover(table):
+    conn = sqlite3.connect('MACRO_ECONOMIC_DATA.db')
+
+    query = f'SELECT * FROM {table} ORDER BY date ASC'
+
+    df = pd.read_sql_query(query, conn)
+    df['close'] = df['close'].astype(float)
+    # Calculating SMA (Smple Moving Average)
+    sma_one = 20
+    sma_two = 50
+
+    sma1 = df['close'].rolling(sma_one).mean()
+    print('The SMA one: \n', type(sma1)) #pandas series
+
+    sma2 = df['close'].rolling(sma_two).mean()
+    print('The SMA two: \n', type(sma2)) #pandas series
+
+    return sma1,sma2
+
+def build_ema(table):
+    conn = sqlite3.connect('MACRO_ECONOMIC_DATA.db')
+
+    query = f'SELECT * FROM {table} ORDER BY date ASC'
+
+    df = pd.read_sql_query(query, conn)
+    df['close'] = df['close'].astype(float)
+    
+    # Calculating EMA (Exponential Moving Average)
+    ema_period = 20
+    ema = df['close'].rolling(ema_period).mean()
+
+    return ema
+
+
+def build_bands(table):
     conn = sqlite3.connect('MACRO_ECONOMIC_DATA.db')
 
     query = f'SELECT * FROM {table} ORDER BY date ASC'
@@ -15,19 +49,8 @@ def build_calcs_table(table):
 
     # Step 3: Convert string values to numbers
     df['close'] = df['close'].astype(float)
-    df['high'] = df['high'].astype(float)
-    df['open'] = df['open'].astype(float)
-    df['low'] = df['low'].astype(float)
 
-    # Calculating SMA (Smple Moving Average)
-    sma_period = 20
-    df['sma'] = df['close'].rolling(sma_period).mean()
-
-     # Calculating EMA (Exponential Moving Average)
-    ema_period = 20
-    df['ema'] = df['close'].rolling(ema_period).mean()
-
-    #  Calculating Bolinger bands
+    # #  Calculating Bolinger bands
     bb_period = 20
     df['std'] = df['close'].rolling(bb_period).std()
     df['upper_band'] = df['sma'] + 2 * df['std']
@@ -39,3 +62,6 @@ def build_calcs_table(table):
 
 
     conn.close()
+
+
+build_sma_crossover('NASDAQ_COMPOSITE_INDEX')
