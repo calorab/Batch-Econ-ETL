600f7114435f8ddf96033ef190c4e1cee5a09f06
Calorab <calebwgammon@gmail.com>
2023-06-22 14:04:35 -0400

    hardcoded econ table in, needs formatting

--- charts.py
+++ charts.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import sqlite3
 from sqlite3 import OperationalError, Error
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px # for creating figures for graphs
 
 
@@ -16,27 +16,27 @@ def get_data():
     try:
         conn = sqlite3.connect('MACRO_ECONOMIC_DATA.db')
     except Error as err:
-        print('Database Connection error \n', err)
-    
-    df = pd.read_sql("SELECT date, value FROM COMMODITIES_INDEX WHERE date >= '2003-01-01' ORDER BY date ASC ", conn)
-    df['value'] = df['value'].astype(float)
+        print('Connection error \n', err)
+
+    query = '''SELECT * FROM CONSUMER_ECON_VW'''
+    df = pd.read_sql_query(query, conn) 
     
     conn.close()
     return df
 
 
 def build_layout():
-    df = get_data()
+    # df = get_data()
     app = Dash(__name__)
 
     app.layout = html.Div([ 
-        html.Div(Children=[
-            html.label('Consumer Economic Indicators'),
-            # insert table function here
+        html.Div([
+            html.Label('Consumer Economic Indicators'),
+            build_econ_table(),
 
             html.Br(),
-            html.label('Banking Indicators'),
-            # insert T/Fed graph
+            html.Label('Banking Indicators'),
+            build_fed_graph(),
         ], style={'padding': 10, 'flex': 1}),
 
         html.Div([
@@ -44,10 +44,11 @@ def build_layout():
             dcc.Checklist([]), # CALEB checklist items here - must be list!!!
         ], style={'padding': 10, 'flex': 1}) 
            
-    ],style={'display': 'flex', 'flex-direction': 'row'})
+    ], style={'display': 'flex', 'flex-direction': 'row'})
 
     app.run_server(debug=True)
     # app.run_server(dev_tools_hot_reload=False) to remove hot-reloading
+    # The location of the app: http://127.0.0.1:8050/ 
 
 
 
@@ -58,18 +59,16 @@ def build_econ_table():
     except Error as err:
         print('Connection error \n', err)
 
-    df = pd.read_sql_table('CONSUMER_ECON_VW', conn) # may need change to read_sql_query()
+    query = '''SELECT * FROM CONSUMER_ECON_VW'''
+    df = pd.read_sql_query(query, conn)
+    # df['Value'] = df['Value'].astype(float)
     # Need a figure (px.[graph-type])
     return html.Table([
                 html.Thead(
-                    html.Tr([html.Th(col) for col in df.columns])
+                    html.Tr([html.Td(df.columns[0]), html.Td(df.columns[1])])
                 ),
-                html.Tbody([
-                    html.Tr([
-                        html.Td(df.iloc[i][col]) for col in df.columns
-                    ]) for i in range(len(df)) # may need changes
-                ])
-            ]), # CALEB Inflation, GDP, and CPI most recent numbers (more??)
+                html.Tbody([html.Tr([html.Th(df.iloc[0][0]), html.Td(df.iloc[0][1])]), html.Tr([html.Th(df.iloc[1][0]), html.Td(df.iloc[1][1])]), html.Tr([html.Th(df.iloc[2][0]), html.Td(df.iloc[2][1])])])
+                ], style={'padding': 10, 'flex': 1}) # CALEB Need to turn this into a loops or something eventually
 
 
 def build_fed_graph():
@@ -79,19 +78,19 @@ def build_fed_graph():
     except Error as err:
         print('Connection error \n', err)
     
-    query = '''SELECT * FROM FED_RATES_VW ORDER BY date ASC'''
+    query = '''SELECT * FROM FED_RATES_VW ORDER BY date DESC LIMIT 150'''
     df = pd.read_sql_query(query, conn)
     df['value'] = df['value'].astype(float)
 
-    # Need a figure (px.[graph-type])
-    fig = px.line(df, x='date', y=range(0, 12) color='indicator') # was error here not sure why
-    return dcc.Graph([]) #CALEB T-Yield and Fed Funds Rate
+    # Need a figure
+    fig = px.line(df, x='date', y='value', color='indicator') # CALEB - NEEDS TESTING**
+    return dcc.Graph(figure=fig, id='econ-graph-small') # CALEB T-Yield and Fed Funds Rate
 
 def build_interactive_graph():
     # need funcs for callbacks
     # need figure for graph
     # need data from 
-    return dcc.Graph([]), 
+    return dcc.Graph(), 
 
 
 build_dashboard()
