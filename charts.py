809939c346600429da78164b87b4f1af3e00610e
Calorab <calebwgammon@gmail.com>
2023-06-26 15:12:56 -0400

    added basic styling - need more

--- charts.py
+++ charts.py
@@ -5,6 +5,8 @@ from sqlite3 import OperationalError, Error
 from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px 
 from mapping import field_mapping as map
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 #  CALEB - you need ECON data, CONSUMER data and INTERACTIVE data
 
@@ -49,8 +51,9 @@ def build_dropdown():
     return options, options_map
     
 
-app = Dash(__name__)
-
+app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+load_figure_template("LUX")
+                     
 bank_df = get_bank_data()
 cons_df = get_cons_data()
 bank_figure = px.line(bank_df, x='date', y='value', color='indicator')
@@ -59,25 +62,25 @@ dp_options, dp_dict = build_dropdown()
 
 app.layout = html.Div([
     html.Div([
-        html.Label('Consumer Economic Indicators'),
+        html.Label('Consumer Economic Indicators', style={'font-weight': 'bold'}),
         html.Table([
             html.Thead(
                 html.Tr([html.Td(cons_df.columns[0]), html.Td(cons_df.columns[1])])
             ),
             html.Tbody([html.Tr([html.Th(cons_df.iloc[0][0]), html.Td(cons_df.iloc[0][1])]), html.Tr([html.Th(cons_df.iloc[1][0]), html.Td(cons_df.iloc[1][1])]), html.Tr([html.Th(cons_df.iloc[2][0]), html.Td(cons_df.iloc[2][1])])])
-            ], style={'padding': 10, 'flex': 1}), # CALEB Need to turn this into a loop or something eventually,
+            ], style={'padding': 10, 'flex': 1, 'textAlign': 'left'}), # CALEB Need to turn this into a loop or something eventually,
 
         html.Br(),
-        html.Label('Banking Indicators'),
+        html.Label('Banking Indicators', style={'font-weight': 'bold'}),
         dcc.Graph(id='econ-graph-small', figure=bank_figure)
-    ], style={'padding': 10, 'flex': 1}),
+    ], style={'padding': 10, 'flex': 1, 'width': '30%'}),
 
     html.Div([
         dcc.Dropdown(dp_options, 'S&P 500', id='main-dropdown'),
         dcc.Graph(id='main-graph') #update function that returns a figure 
-    ], style={'padding': 10, 'flex': 1}) 
+    ], style={'padding': 20, 'flex': 1, 'padding-top': 50}) 
         
-], style={'display': 'flex', 'flex-direction': 'row'})
+], style={'display': 'flex', 'flex-direction': 'row', 'padding': 20, 'margin': 40,'border-style': 'solid', 'border-color': 'grey', 'background-color': 'dark-grey'})
 
 
 @callback(
@@ -95,9 +98,9 @@ def update_interactive_graph(index):
     query = f'SELECT * FROM {table} ORDER BY date ASC'
     df = pd.read_sql_query(query, conn)
 
-    # y_axis = None
+    y_axis = None
     if index in ['WTI OIL', 'Commodities Index']:
-        y_axis = 'values'
+        y_axis = 'value'
     else:
         y_axis = 'close'
     
